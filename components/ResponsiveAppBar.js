bd886cff3576e51383ff3e1ac0ef48d389400779
Abhyuday <abhyuday911@gmail.com>
2023-08-29 20:06:27 +0530

    70% complete

---
+++ components/ResponsiveAppBar.js
@@ -0,0 +1,132 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import styled from "@emotion/styled";
+
+const pages = [
+  "SIGN IN",
+  "LEGAL",
+  "LICENCES",
+  "SECURITY",
+  "CAREERS",
+  "PRESS",
+  "SUPPORT",
+  "STATUS",
+  "CODEBLOG",
+];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const EyeImageContainer = styled("div")({
+  backgroundImage: 'url("/eye.svg")',
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  width: "max(80px, 2vw)",
+  aspectRatio: "1.8",
+  display: "flex",
+  justifyContent: "center",
+  padding: 0,
+  cursor: "pointer",
+});
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ bgcolor: "transparent" }} elevation={0}>
+      <Container
+        maxWidth="xl"
+        sx={{ bgcolor: "transparent", backgroundImage: "transparent" }}
+      >
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: {xs:1, lg:0}, display: { xs: "flex"} }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: {
+                xs: "none",
+                sm: "none",
+                md: "none",
+                lg: "flex",
+                justifyContent: "center",
+              },
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  mx: { md: 0, lg: 1 },
+                  color: "white",
+                  display: "block",
+                }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Eye of the tiger">
+              <EyeImageContainer />
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,123 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import styled from "@emotion/styled";
+import {
+  Box,
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
+import React from "react";
+import ResponsiveAppBar from "../../components/ResponsiveAppBar";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const BackgroundImageContainer = styled("div")({
+  backgroundImage: 'url("/CircularVector.png")',
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  width: "100vw",
+  height: "100vh",
+  overflow: "hidden",
+  display: "flex",
+  justifyContent: "center",
+  // alignItems: 'center',
+  padding: 0,
+  position: "relative"
+});
+
+const BackgroundPhoneImageContainer = styled("div")({
+  backgroundImage: 'url("/Phone.png")',
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  height: "50vh",
+  aspectRatio: "0.8",
+  position: "absolute",
+  top: "-90%",
+  left: "50%",
+  transform: "translate( -50%, 0% )",
+  padding: 0,
+});
+
+const BackgroundStairsImageContainer = styled("div")({
+  backgroundImage: 'url("/Stairs.png")',
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  height: "30vh",
+  aspectRatio: "0.8",
+  position: "absolute",
+  top: "-150%",
+  right: "-50%",
+  transform: "translate( 0%, 0% )",
+  padding: 0,
+});
+
+const BackgroundPillarImageContainer = styled("div")({
+  backgroundImage: 'url("/Pillar.png")',
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  height: "30vh",
+  aspectRatio: "3.4/2.8",
+  position: "absolute",
+  bottom: "0%",
+  right: "20%",
+  padding: 0,
+});
+
+const page = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BackgroundImageContainer>
+        <Container maxWidth="xl">
+          <ResponsiveAppBar />
+          <Box
+            sx={{
+              position: "absolute",
+              top: "40%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <BackgroundStairsImageContainer/>
+            <Typography
+              align="center"
+              sx={{ fontWeight: 900, fontSize: 200, lineHeight: 0.5 }}
+              variant="h1"
+              component={"h2"}
+            >
+              CASH
+            </Typography>
+            <BackgroundPhoneImageContainer />
+            <Typography
+              align="center"
+              sx={{
+                fontWeight: 900,
+                fontSize: 200,
+                position: "absolute",
+                zIndex: 2,
+                top: "100%",
+                left: "50%",
+                transform: "translate( -50%, 0% )",
+              }}
+              variant="h1"
+              component={"h2"}
+            >
+              APP
+            </Typography>
+          </Box>
+        </Container>
+        <BackgroundPillarImageContainer/>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      </BackgroundImageContainer>
+    </ThemeProvider>
+  );
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default page;
